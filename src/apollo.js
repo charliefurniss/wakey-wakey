9fd89e519018f8c5b55a3b5c78211be83705b13d
Charlie Furniss <charliefurniss96@gmail.com>
2020-01-13 16:37:30 +0000

    Fetching Air quality data with GraphQl

--- src/apollo.js
+++ src/apollo.js
@@ -1,5 +1,7 @@
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export default new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
 });
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,20 @@ const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const gql = require('graphql-tag');
 const { buildASTSchema } = require('graphql');
+const fetch = require('node-fetch');
+
+const getAirQuality = async () => {
+  const airQualityData = await fetch('https://api.tfl.gov.uk/airquality')
+    .then(res => res.json())
+    .then(json => json);
+
+  const forecast = {
+    band: airQualityData.currentForecast[0].forecastBand,
+    summary: airQualityData.currentForecast[0].forecastSummary
+  };
+
+  return forecast;
+};
 
 const POSTS = [
   { author: 'Charlie Furniss', body: 'this is it' },
@@ -13,6 +27,7 @@ const schema = buildASTSchema(gql`
   type Query {
     posts: [Post]
     post(id: ID!): Post
+    air: AirQuality
   }
 
   type Post {
@@ -20,13 +35,19 @@ const schema = buildASTSchema(gql`
     author: String
     body: String
   }
+
+  type AirQuality {
+    band: String
+    summary: String
+  }
 `);
 
 const mapPost = (post, id) => post && { id, ...post };
 
 const root = {
   posts: () => POSTS.map(mapPost),
-  post: ({ id }) => mapPost(POSTS[id], id)
+  post: ({ id }) => mapPost(POSTS[id], id),
+  air: () => getAirQuality()
 };
 
 const app = express();
