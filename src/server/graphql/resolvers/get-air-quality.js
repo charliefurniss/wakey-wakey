8523d50926c922aec69b7e545a48b09a1439aa08
Charlie Furniss <charliefurniss96@gmail.com>
2020-01-28 15:17:04 +0000

    Tidying up

--- src/server/graphql/resolvers/get-air-quality.js
+++ src/server/graphql/resolvers/get-air-quality.js
@@ -4,12 +4,10 @@ const getAirQuality = async () => {
   const result = await fetch('https://api.tfl.gov.uk/airquality');
   const airQualityData = await result.json();
 
-  const forecast = {
+  return {
     band: airQualityData.currentForecast[0].forecastBand,
     summary: airQualityData.currentForecast[0].forecastSummary
   };
-
-  return forecast;
 };
 
 module.exports = getAirQuality;
