6ffb3ee9548b8b82d44522b0df46e36785be5205
Charlie Furniss <charliefurniss96@gmail.com>
2020-04-09 14:53:49 +0100

    Adding colours and updating primary and secondary

--- src/components/utilities/core-colours.js
+++ src/components/utilities/core-colours.js
@@ -1,10 +1,15 @@
 export const colours = {
   green: '#4caf50',
+  yellow: '#fff200',
   amber: '#ff9800',
   red: '#ff5722',
   purple: '#673ab7',
+  lightGrey: '#d8d9da',
+  darkGrey: '#757575',
+  white: '#FFFFFF',
   black: '#000000',
   primaryBackground: '#171819',
   lighterBackground: '#212124',
-  primaryText: '#d8d9da'
+  primaryText: '#d8d9da',
+  secondaryText: '#212124'
 };
