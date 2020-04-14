6a81def26d866750b57761e252e916a9e1485d69
Charlie Furniss <charliefurniss96@gmail.com>
2020-04-14 17:18:57 +0100

    Adding check round TravelInformation component

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ import NavBar from './components/nav-bar';
 import AirQuality from './components/air-quality';
 import PollenCount from './components/pollen-count';
 import TravelInformation from './components/travel-information';
+import WeatherForecast from './components/weather-forecast';
 import Drawer from './components/drawer';
 
 function App() {
   const [linesToCheck, setLinesToCheck] = useState([]);
 
-  const handleLineCheckboxCheck = lineId => {
+  const handleLineCheckboxCheck = (lineId) => {
     if (!linesToCheck.includes(lineId)) {
       setLinesToCheck([...linesToCheck, lineId]);
     } else {
       const filteredLinesToCheck = linesToCheck.filter(
-        lineIdToCheck => lineIdToCheck !== lineId
+        (lineIdToCheck) => lineIdToCheck !== lineId
       );
       setLinesToCheck(filteredLinesToCheck);
     }
   };
-
-  useEffect(() => {}, [linesToCheck]);
 
   return (
     <div className='app-layout mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-drawer'>
@@ -33,7 +32,10 @@ function App() {
         <PageContent>
           <AirQuality />
           <PollenCount />
-          <TravelInformation />
+          {linesToCheck.length > 0 && (
+            <TravelInformation linesToCheck={linesToCheck.join()} />
+          )}
+          <WeatherForecast />
         </PageContent>
       </StyledMain>
     </div>
